e6ae08bcc2ca6fa54b190efbd4f7a17f5bccb98e
script <87186934+Annabeth121993@users.noreply.github.com>
2022-04-27 00:44:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var turtle , cheetah , shield;
+
+function preload(){
+      turtle=loadImage("slowturtle.png",slow);
+      cheetah=loadImage("fastcheetah.png",fast);
+      shield=loadImage("safeshield.png",safe);  
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+ if () {
+       
+ }
+}
